4a1ae64fbd0b97968a8ce8da9091372d6e91eaaa
David Garcia <jdcupn@gmail.com>
2023-02-15 15:02:34 -0500

    tipos de datos con variables

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 print("Hola mundo!!!")
 #Tipo de dato
 print(type("Hola mundo!!!"))
+#Tipos de datos
+a = 1
+print(type(a))
+b = 2.33
+print(type(b))
+c = True
+print(type(c))
+d = "c"
+print(type(d))
+e = "Algoritmos"
+print(type(e))
