7a94d2b8ee71cdbffcda6608f302ff88c95ef715
David Garcia <jdcupn@gmail.com>
2023-02-16 15:15:27 -0500

    Se agrego diferencia

--- conjuntos.py
+++ conjuntos.py
@@ -1,7 +1,19 @@
+"""
+#Llibreria
+import random
 #Definición de conjuntos
-A = {1,2,3,4,5,6}
-B = {5,6,7,8}
-C = {6,7,8,9,10}
+# Generar un conjunto aleatorio de 10 números enteros del 1 al 100
+A = set(random.sample(range(1, 101), 10))
+B = set(random.sample(range(1, 101), 10))
+C = set(random.sample(range(1, 101), 10))
+#Datos
+print(f"Conjunto A: {A}")
+print(f"Conjunto B: {B}")
+print(f"Conjunto C: {C}")
+
+#Operación 1
+R = B.intersection(C)
+print(R)
 
 #Operación 1
 R = A.intersection(B)
@@ -16,3 +28,16 @@ print(RT)
 # & -----> iINTERSECCION
 RT1 = ((A & B)& C)| B
 print(RT1)
+"""
+#Ejercicio 1
+A = {1,2,3,4,5}
+B = {4,5,6}
+C = {5,6,7,8,9,10}
+D = {10,11,12}
+
+RT = A.difference(B)
+print(RT)
+
+RT1 = (((A-B)|(C-B)) & (C-B)) & (B-C)
+print(RT1)
+
