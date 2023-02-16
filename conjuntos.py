b459aac097c2fb4ec82b85a23e2645bcd99347a9
David Garcia <jdcupn@gmail.com>
2023-02-16 14:24:38 -0500

    Tipos de conjuntos(Union e Interseccion)

--- conjuntos.py
+++ conjuntos.py
@@ -1,4 +1,18 @@
 #Definición de conjuntos
-A = {}
-B = {}
-#Usuario
+A = {1,2,3,4,5,6}
+B = {5,6,7,8}
+C = {6,7,8,9,10}
+
+#Operación 1
+R = A.intersection(B)
+print(R)
+#Operación 2
+Z = R.intersection(C)
+print(Z)
+#Ooperacion 3
+RT = Z.union(B) ## Z|B
+print(RT)
+# | -----> Union
+# & -----> iINTERSECCION
+RT1 = ((A & B)& C)| B
+print(RT1)
