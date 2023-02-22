933b4f8efa083f59a27551fb1ab8e20758fa6f0e
David Garcia <jdcupn@gmail.com>
2023-02-22 15:27:34 -0500

    Calculadora de suma de dos numeros int

---
+++ operadores aritmericos.py
@@ -0,0 +1,10 @@
+##Adquiriri informacion 
+#Entradas
+a = int(input("Digite el numero 1: "))
+print(f"El numero a es: {a}")
+b = int(input("Digite el numero 2: "))
+print(f"El numero a es: {b}")
+#Proceso
+c = a + b
+#Salida
+print(f"El resultado de la suma es : {c}")
