f272e773d817198f071aff556e5af6ee3eb90bb1
David Garcia <jdcupn@gmail.com>
2023-02-22 15:54:03 -0500

    Calculadore version 2, se agrego operaciones y uso de librerias

--- operadores aritmericos.py
+++ operadores aritmericos.py
@@ -1,3 +1,5 @@
+#Libreria
+import math
 ##Adquiriri informacion 
 #Entradas
 a = int(input("Digite el numero 1: "))
@@ -6,5 +8,15 @@ b = int(input("Digite el numero 2: "))
 print(f"El numero a es: {b}")
 #Proceso
 c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a ** b
+h = math.sqrt(a)
 #Salida
 print(f"El resultado de la suma es : {c}")
+print(f"El resultado de la resta es : {d}")
+print(f"El resultado de la multiplicación es : {e}")
+print(f"El resultado de la suma división : {f}")
+print(f"El resultado de la potencia : {g}")
+print(f"El resultado de la raiz de a es : {h}")
