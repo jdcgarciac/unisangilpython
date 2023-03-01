855c4efa69e2e474e3be49ca35d11dd4a21e3241
David Garcia <jdcupn@gmail.com>
2023-03-01 13:53:24 -0500

    Calculadora de sistemas numericos

---
+++ sis_numericos.py
@@ -0,0 +1,10 @@
+numero = int(input("Digite el numero a transformar: "))
+#Conv. decimal-binario
+binario = bin(numero)
+print(f"Binario: {binario}")
+#Conv. decimal-hexa
+hexadecimal = hex(numero)
+print(f"Hexadecimal: {hexadecimal}")
+#Conv. decimal-octal
+octal = oct(numero)
+print(f"Octal: {octal}")
