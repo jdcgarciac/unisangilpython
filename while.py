3f4791ee1bc322fac4e777e6cf2098c3a02426f7
David Garcia <jdcupn@gmail.com>
2023-03-08 14:23:14 -0500

    funciones con string

--- while.py
+++ while.py
@@ -1,10 +1,29 @@
+"""
 #Variable
 contador = 0
 
 #Estructuta
 while contador <=4:
+    if contador == 3:
+        print("Unisangil")
+        break
     print(f"contador = {contador}")
     contador += 1
 print("Salio de while")
-
 #Break y continue
+#Ejemplo
+while contador < 20:
+    contador += 1
+    if contador < 15:
+        continue
+    print(contador)
+"""
+texto = "Estoy en clase de algoritmos en unisangil"
+#Buscar
+print("clases" in texto)
+print(texto.title())#titulo
+print(texto.upper())#Mayusculas
+print(texto.lower())#Minusculas
+print(texto.count("a"))#Mayusculas
+
+
