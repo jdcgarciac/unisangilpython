30b3ad70141765dd6c79547f7778c5a9d6f1edc2
David Garcia <jdcupn@gmail.com>
2023-03-08 14:00:19 -0500

    Se agrego while

--- Juego_P_P_T.py
+++ Juego_P_P_T.py
@@ -2,30 +2,32 @@
 import random
 #Opciones
 op = ("Piedra","Papel","Tijera")
-#Entradas
-usuario = input("Digite la opción Pierdra, Papel o Tijera:  ")
-cpu = random.choice(op)
-#Imprimir informacion
-print(f"La op que digito el usuario es: {usuario}")
-print(f"La op CPU es: {cpu}")
-#Proceso
-if usuario == "Piedra" and cpu == "Piedra":
-    print("Empate!!!")
-elif usuario == "Tijera" and cpu == "Tijera":
-    print("Empate!!!")
-elif usuario == "Papel" and cpu == "Papel":
-    print("Empate!!!")
-elif usuario == "Piedra" and cpu == "Papel":
-    print("Gana cpu!!!")
-elif usuario == "Piedra" and cpu == "Tijera":
-    print("Gana usuario!!!")
-elif usuario == "Papel" and cpu == "Piedra":
-    print("Gana usuario!!!")
-elif usuario == "Papel" and cpu == "Tijera":
-    print("Gana cpu!!!")
-elif usuario == "Tijera" and cpu == "Piedra":
-    print("Gana cpu!!!")
-elif usuario == "Tijera" and cpu == "Papel":
-    print("Gana usuario!!!")
-else:
-    print("Error!!!")
+#Estructura while
+while True:
+    #Entradas
+    usuario = input("Digite la opción Pierdra, Papel o Tijera:  ")
+    cpu = random.choice(op)
+    #Imprimir informacion
+    print(f"La op que digito el usuario es: {usuario}")
+    print(f"La op CPU es: {cpu}")
+    #Proceso
+    if usuario == "Piedra" and cpu == "Piedra":
+        print("Empate!!!")
+    elif usuario == "Tijera" and cpu == "Tijera":
+        print("Empate!!!")
+    elif usuario == "Papel" and cpu == "Papel":
+        print("Empate!!!")
+    elif usuario == "Piedra" and cpu == "Papel":
+        print("Gana cpu!!!")
+    elif usuario == "Piedra" and cpu == "Tijera":
+        print("Gana usuario!!!")
+    elif usuario == "Papel" and cpu == "Piedra":
+        print("Gana usuario!!!")
+    elif usuario == "Papel" and cpu == "Tijera":
+        print("Gana cpu!!!")
+    elif usuario == "Tijera" and cpu == "Piedra":
+        print("Gana cpu!!!")
+    elif usuario == "Tijera" and cpu == "Papel":
+        print("Gana usuario!!!")
+    else:
+        print("Error!!!")
