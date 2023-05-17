894a23d614710bffbcbd7f6a323fc6972a365008
David García <jdgarcia1@unisangil.edu.co>
2023-05-17 15:46:30 -0500

    arreglado

--- Base de datos/excel_bd.py
+++ Base de datos/excel_bd.py
@@ -18,7 +18,7 @@ conexion = mysql.connector.connect(
 #Crear un cursor para generar consultas
 cursor = conexion.cursor()
 #Insertar datos
-for index,row in df.head(150).iterrows:
+for index,row in df.head(150).iterrows():
     #Generar consultas en sql
     sql = "INSERT INTO datos (Id,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Species) VALUES(%s,%s,%s,%s,%s,%s)"
     datos = (row["Id"],row["SepalLengthCm"],row["SepalWidthCm"],row["PetalLengthCm"],row["PetalWidthCm"],row["Species"])
